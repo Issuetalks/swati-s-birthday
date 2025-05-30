60d61405ee6f86f8810f173209ca6cb765dccfe7
Issuetalks <ikishu009@gmail.com>
2025-05-30 12:35:05 +0530

    Add files via upload

---
+++ swati.js
@@ -0,0 +1,11 @@
+// Start muted (allowed by browsers)
+bgMusic.muted = true;
+bgMusic.play()
+    .then(() => {
+        // After play starts, try unmuting
+        bgMusic.muted = false;
+    })
+    .catch(error => {
+        // Show play button if failed
+        playButton.style.display = "block";
+    });
